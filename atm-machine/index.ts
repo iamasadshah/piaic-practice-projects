98d47d4bf2c7ca7db05f11d2b87411086a19512f
Syed Asadullah Shah <135575639+iamasadshah@users.noreply.github.com>
2024-04-01 13:43:44 +0500

    atm-machine-ts

---
+++ atm-machine/index.ts
@@ -0,0 +1,39 @@
+import inquirer from "inquirer";
+
+let pin: number = 1234;
+let Balance = 10000;
+
+inquirer.prompt({
+    type: 'number',
+    name: 'yourPin',
+    message: 'Enter your Pin'
+}).then((answers) => {
+    let yourPin = answers.yourPin;
+
+    if (yourPin === pin) {
+        console.log("Correct PIN");
+
+        inquirer.prompt({
+            type: "list",
+            name: "selection",
+            message: "Please Select Option",
+            choices: ["Check balance", "Withdraw"]
+        }).then((answer) => {
+            if (answer.selection === "Check balance") {
+                console.log(`Your Current Balance is ${Balance}`);
+            } else if (answer.selection === "Withdraw") {
+                inquirer.prompt({
+                    type: "number",
+                    name: "selection2",
+                    message: `Your Current balance is ${Balance}. Please Enter Amount to Withdraw.`
+                }).then((answer2) => {
+                    if (answer2.selection2 <= Balance){
+                      console.log(`You Witdraw ${answer2.selection2}. \n \n Your Remaining Balance is ${Balance - answer2.selection2}`);
+                    }else if (answer2.selection2 != Balance){
+                      console.log(`Insufficient balance. Please enter an amount within the range of ${Balance}`);
+                    }
+                });
+            }
+        });
+    }
+});
